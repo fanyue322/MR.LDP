80c00775453a50433fb10a871a08a44412f47b73
QingCheng0218 <51993787+QingCheng0218@users.noreply.github.com>
2020-02-28 15:12:46 +0800

    Update ValidationStudy.R

--- Simulation and Real Data Analysis/ValidationStudy.R
+++ Simulation and Real Data Analysis/ValidationStudy.R
@@ -1,6 +1,8 @@
 rm(list = ls());
-library(MR.LDP)
-
+library(MR.LDP);
+library("MendelianRandomization");
+library(mr.raps);
+library(gsmr)
 # For CAD-CAD study.
 filescreen= "heart attack_myocardial infarction.txt";
 fileexposure = "c4d.txt";
@@ -13,6 +15,7 @@ fileoutcome = "cardiogram.txt";
 # -------------------------------------------------
 stringname3 = "all_chr_1000G";
 block_file = "fourier_ls-all.bed";
+nrefname3 = paste0(stringname3,".fam");
 
 QC.pa = c(1, 0);
 LAM.pa = c(0.1, 0.15);
@@ -23,8 +26,8 @@ for(i in 1:nrow(para.all)){
 }
 
 para.iter <- res1;
-QCindex = as.numeric(res1[1]);
-lam <- as.numeric(res1[2]);
+QCindex <-  0;
+lam <- 0.1;
 
 filename = paste0("cad_cad","Lam", lam*100, stringname3, "QC", QCindex, ".Rdata");
 
@@ -44,6 +47,9 @@ avbIndex = scrres$idxin;
 idx4panel = scrres$idx4panel;
 
 screendata = read.table(filescreen, header = T);
+expdata = read.table(fileexposure, header = T);
+outdata = read.table(fileoutcome, header = T);
+
 # ------------------------------------------------------------------------------
 # Select instrumental SNPs using screening data under different thresholds
 idx = match(rsname, screendata$SNP);
@@ -54,8 +60,14 @@ PVA = c(5e-06, 1e-05, 5e-05, 1e-04, 5e-04, 1e-03, 5e-03, 1e-02)
 pres = rep(1, length(PVA));
 bres = rep(1, length(PVA));
 Tstat = rep(1, length(PVA));
-bhat = se = tstatres = matrix(0, ncol = 2, nrow = length(PVA));
+bhat = se = tstatres = matrix(0, ncol = 3, nrow = length(PVA));
 MHCpres = Xpres = Ypres = rep(1, length(PVA));
+
+
+LDR2 = c(0.001, 0.05);
+Bres = Sres = array(0, dim = c(length(LDR2), 5, length(PVA)));
+Nused = Nldres = Pres = matrix(0, nrow = length(LDR2), ncol = length(PVA));
+pres = rep(0, length(PVA));
 
 for(irep in 1:length(PVA)){
   
@@ -156,16 +168,110 @@ for(irep in 1:length(PVA)){
     bhat[irep, 2] = result0$beta0;
     se[irep, 2] = bhat[irep, 1]/sqrt(Tstat);
     tstatres[irep, 2] = Tstat;
+    
+    #-------------------------------------------------------------------------------------
+    # The linear regression method.
+    fit = summary(lm(bh2new~bh1new))
+    bhat[irep, 3] <- fit$coefficients[2, 1];
+    se[irep, 3] <- fit$coefficients[2, 2];
     #-------------------------------------------------------------------------------------
     
   }
+  
+  
+  # ------------------------------------------------------------------------------
+  # obtain the pvalue of exposure data and outcome data
+  id_x = match(rsnamenew, expdata$SNP);
+  exp_in = expdata[id_x, ];
+  pval_x = exp_in$pvalue;
+  id_y = match(rsnamenew, outdata$SNP);
+  out_in = outdata[id_y, ];
+  pval_y = out_in$pvalue;
+  # ------------------------------------------------------------------------------
+  
+  
+  Rblockres = Cal_block_Rmatrix(bpnew, chrnew, avbIndexnew-1,idx4panel, block_file, stringname3, coreNum, lam);
+  
+  Rnew = Rblockres$R
+  n_ref = getLineNum(nrefname3);
+
+  colnames(Rnew) = rsnamenew;
+  rownames(Rnew) = rsnamenew;
+  snp_coeff_id = rsnamenew;
+  
+  
+  gwas_thresh = 1   # GWAS threshold to select SNPs as the instruments for the GSMR analysis
+  single_snp_heidi_thresh = 0.01    # p-value threshold for single-SNP-based HEIDI-outlier analysis
+  multi_snp_heidi_thresh = 0.01    # p-value threshold for multi-SNP-based HEIDI-outlier analysis
+  nsnps_thresh = 1   # the minimum number of instruments required for the GSMR analysis
+  heidi_outlier_flag = T    # flag for HEIDI-outlier analysis
+  
+  ld_fdr_thresh = 0.05   # FDR threshold to remove the chance correlations between the SNP instruments
+  gsmr_beta = 0     # 0 - the original HEIDI-outlier method; 1 - the new HEIDI-outlier method that is currently under development 
+  
+  bhat5 = se5 = matrix(0, ncol = 5, nrow = length(LDR2))
+  # bhat = se = nbres = rep(0, length(LDR2));
+  nld = nused = rep(0, length(LDR2));
+  
+  for(rr in 1:length(LDR2)){
+    
+    ld_r2_thresh = LDR2[rr]   # LD r2 threshold to remove SNPs in high LD
+    
+    gsmr_results = gsmr(bh1new, s12new, pval_x, bh2new, s22new, pval_y,
+                        Rnew, snp_coeff_id, n_ref, heidi_outlier_flag, 
+                        gwas_thresh, single_snp_heidi_thresh, multi_snp_heidi_thresh, 
+                        nsnps_thresh, ld_r2_thresh, ld_fdr_thresh, gsmr_beta) 
+    
+    
+    bhat5[rr, 1] = gsmr_results$bxy;
+    se5[rr, 1] = gsmr_results$bxy_se;
+    nld[rr] = p - length(gsmr_results$linkage_snps);
+    nused[rr] = length(gsmr_results$used_index);
+    
+    id00 = match(gsmr_results$linkage_snps, rsnamenew);
+    idx = 1:p;
+    id4ld = idx[-id00];
+    
+    bh14ld = bh1_in[id4ld];
+    bh24ld = bh2_in[id4ld];
+    s124ld = s12_in[id4ld];
+    s224ld = s22_in[id4ld];
+    
+    #-------------------------------------------------------------------------------------
+    # The linear regression method.
+    fit = summary(lm(bh24ld~bh14ld))
+    bhat5[rr, 2] <- fit$coefficients[2, 1];
+    se5[rr, 2] <- fit$coefficients[2, 2];
+    #-------------------------------------------------------------------------------------
+    # The Raps method.
+    raps = mr.raps(bh14ld, bh24ld, s124ld, s224ld);
+    bhat5[rr, 3] <- raps$beta.hat;
+    se5[rr, 3] <- raps$beta.se;
+    #-------------------------------------------------------------------------------------
+    # The inverse-variance weighted method.
+    ivm.res <- mr_ivw(mr_input(bx = bh14ld, bxse = s124ld, by = bh24ld, byse = s224ld));
+    bhat5[rr, 4] <- ivm.res$Estimate;
+    se5[rr, 4] <- ivm.res$StdError;
+    # -------------------------------------------------------------------------------------
+    # # The MR-Egger method.
+    mregger.res <- mr_egger(mr_input(bx = bh14ld, bxse = s124ld, by = bh24ld, byse = s224ld));
+    bhat5[rr, 5] <- mregger.res$Estimate;
+    se5[rr, 5] <- mregger.res$StdError.Est;
+  }
+  
+  colnames(bhat5) = colnames(se5) = c("GSMR", "LS","Raps","IVW","MREgger");
+  Bres[, , irep] = bhat5;
+  Sres[, , irep] = se5;
+  Nused[, irep] = nused;
+  Nldres[, irep] = nld;
+  
   
   Ftm <- proc.time();
   S.time <- Ftm[3] - Stm[3];
   print(Sys.time())
   cat( irep,"/",length(PVA)," time: ",S.time,"\n",sep="");
-  
-  colnames(bhat) = colnames(se) = c("MRLD", "MRLDP");
-  save(PVA, pres, MHCpres, Xpres, Ypres, bhat, se, tstatres, file = filename);
+
+  save(PVA, pres, MHCpres, Xpres, Ypres, bhat, se, tstatres,
+       Bres, Sres, pres, Nused, Nldres,file = filename);
   
 }
